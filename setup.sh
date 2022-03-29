ea4292c5affd3c7186dd5d1c0633e573610ca456
frances_brew <40599030+francesfeng@users.noreply.github.com>
2022-03-29 15:50:15 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,9 +5,18 @@ echo "\
 email = \"ffan1201@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
 
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
+
+echo "[theme]
+primaryColor="#1830B7"
+backgroundColor="#FFFFFF"
+secondaryBackgroundColor="#F7F8FA"
+textColor="#212121"
+[runner]
+magicEnabled = false
+[global]
+dataFrameSerialization = "legacy"
+[server]
+headless = true
+port = $PORT
+enableCORS = false
 " > ~/.streamlit/config.toml
